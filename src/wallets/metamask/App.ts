e7a2c8ce9b83be63abd8466ccd00d050d000a059
AzerothA <telifesite@gmail.com>
2022-03-25 00:37:06 +0430

    Some clean code; Class only must created by constructor.

--- src/wallets/metamask/App.ts
+++ src/wallets/metamask/App.ts
@@ -1,19 +1,19 @@
 import { core } from "../index";
 
-declare const window: any;
-
-// Use create() method for create a object
-// Good for UX and security.
 export class Metamask implements core.Application {
 
     wallet: string;
+    window: any;
 
     // creates a metamask object with check
     // If installed or not
-    public static create(): Promise<Metamask> {
-        if (!window.ethereum) return Promise.reject("Metamask is not installed");
+    constructor(window: any) {
+        this.window = window;
+    }
 
-        return Promise.resolve(new Metamask());
+    // It also can return Promise but its good for now
+    public isMetamaskInstalled(): boolean {
+        return Boolean(this.window.ethereum);
     }
 
     /*
@@ -21,7 +21,7 @@ export class Metamask implements core.Application {
      */
     public connect(): Promise<string> {
         // Now recive wallet address from user
-        const wallet = window.ethereum.request({ method: 'eth_requestAccounts' })
+        const wallet = this.window.ethereum.request({ method: 'eth_requestAccounts' })
             .then((accounts: Array<string>) => accounts[0])
             .catch((err: any) => Promise.reject("Cant get wallet address ! Metamask Error: " + err));
 
@@ -41,7 +41,7 @@ export class Metamask implements core.Application {
      * Returns selected chainId
      */
     public chainId(): Promise<number> {
-        const id = window.ethereum.request({ method: 'eth_chainId' })
+        const id = this.window.ethereum.request({ method: 'eth_chainId' })
             .then((id: number) => id)
             .catch((err: any) => Promise.reject(err));
 
