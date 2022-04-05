123b7efc355364993e65cf117a4dfefa7c5e3ae7
AzerothA <telifesite@gmail.com>
2022-04-06 01:04:54 +0430

    New update

--- packages/hector/src/application.ts
+++ packages/hector/src/application.ts
@@ -1,10 +1,10 @@
 export interface Application {
-    /// Wallet address
-    wallet: string;
+  /// Wallet address
+  wallet: string;
 
-    /// Connect to the wallet
-    connect(): Promise<any>;
+  /// Connect to the wallet
+  connect(): Promise<any>;
 
-    /// Returns selected wallets
-    getWallet(): Promise<string>;
+  /// Returns selected wallets
+  getWallet(): Promise<string>;
 }
